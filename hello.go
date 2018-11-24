2d8d8400b27996e5c94c81604d3d47b793a0f044
qingyiwss <365869914@qq.com>
2018-11-24 22:56:26 +0800

    shishi

--- hello.go
+++ hello.go
@@ -9,10 +9,10 @@ type DianShan struct {
 
 func (dianshan *DianShan) open(key bool) string {
 	if key {
-		dianshan.shanye = "转起来了"
+		dianshan.shanye = "转起来了weisongshan"
 
 	} else {
-		dianshan.shanye = "不转了"
+		dianshan.shanye = "不转了50e7f55bd7fb897bfa741acf8d8d65f72d479289"
 	}
 	return dianshan.shanye
 }
